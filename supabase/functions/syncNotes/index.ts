b7c7c45cc2e61d88cff11687d5ab3dd38158336b
Simon Cohen <1simoncohen@gmail.com>
2025-04-27 15:38:08 -0700

    Trigger first deploy

--- supabase/functions/syncNotes/index.ts
+++ supabase/functions/syncNotes/index.ts
@@ -1,3 +1,5 @@
+//trigger deploy
+
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 import { encode as b64 } from "https://deno.land/std@0.168.0/encoding/base64.ts";
